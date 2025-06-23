141c8555d6dbf85cacd2390120bf0da972b327c7
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2025-06-23 19:40:00 -0400

    removing visualization

--- training.py
+++ training.py
@@ -78,63 +78,63 @@ if __name__ == '__main__':
 
     print("saved")
 
-    import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
 
-    dataiter = iter(testloader)
-    images, labels = next(dataiter)
+    # dataiter = iter(testloader)
+    # images, labels = next(dataiter)
 
-    # print images
-    img_grid = torchvision.utils.make_grid(images)
-    # Convert from CHW to HWC format and denormalize
-    img_grid = img_grid.permute(1, 2, 0)
-    img_grid = img_grid * 0.5 + 0.5  # denormalize from [-1, 1] to [0, 1]
-    plt.imshow(img_grid)
-    plt.show()
-    print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
+    # # print images
+    # img_grid = torchvision.utils.make_grid(images)
+    # # Convert from CHW to HWC format and denormalize
+    # img_grid = img_grid.permute(1, 2, 0)
+    # img_grid = img_grid * 0.5 + 0.5  # denormalize from [-1, 1] to [0, 1]
+    # plt.imshow(img_grid)
+    # plt.show()
+    # print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
     
-    net = Net()
-    net.load_state_dict(torch.load(PATH, weights_only=True))
+    # net = Net()
+    # net.load_state_dict(torch.load(PATH, weights_only=True))
 
-    outputs = net(images)
+    # outputs = net(images)
 
-    _, predicted = torch.max(outputs, 1)
+    # _, predicted = torch.max(outputs, 1)
 
-    print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}'
-                              for j in range(4)))
+    # print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}'
+    #                           for j in range(4)))
     
-    correct = 0
-    total = 0
-    # since we're not training, we don't need to calculate the gradients for our outputs
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            # calculate outputs by running images through the network
-            outputs = net(images)
-            # the class with the highest energy is what we choose as prediction
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+    # correct = 0
+    # total = 0
+    # # since we're not training, we don't need to calculate the gradients for our outputs
+    # with torch.no_grad():
+    #     for data in testloader:
+    #         images, labels = data
+    #         # calculate outputs by running images through the network
+    #         outputs = net(images)
+    #         # the class with the highest energy is what we choose as prediction
+    #         _, predicted = torch.max(outputs, 1)
+    #         total += labels.size(0)
+    #         correct += (predicted == labels).sum().item()
 
-    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+    # print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
 
-    # prepare to count predictions for each class
-    correct_pred = {classname: 0 for classname in classes}
-    total_pred = {classname: 0 for classname in classes}
+    # # prepare to count predictions for each class
+    # correct_pred = {classname: 0 for classname in classes}
+    # total_pred = {classname: 0 for classname in classes}
 
-    # again no gradients needed
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            outputs = net(images)
-            _, predictions = torch.max(outputs, 1)
-            # collect the correct predictions for each class
-            for label, prediction in zip(labels, predictions):
-                if label == prediction:
-                    correct_pred[classes[label]] += 1
-                total_pred[classes[label]] += 1
+    # # again no gradients needed
+    # with torch.no_grad():
+    #     for data in testloader:
+    #         images, labels = data
+    #         outputs = net(images)
+    #         _, predictions = torch.max(outputs, 1)
+    #         # collect the correct predictions for each class
+    #         for label, prediction in zip(labels, predictions):
+    #             if label == prediction:
+    #                 correct_pred[classes[label]] += 1
+    #             total_pred[classes[label]] += 1
 
 
-    # print accuracy for each class
-    for classname, correct_count in correct_pred.items():
-        accuracy = 100 * float(correct_count) / total_pred[classname]
-        print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+    # # print accuracy for each class
+    # for classname, correct_count in correct_pred.items():
+    #     accuracy = 100 * float(correct_count) / total_pred[classname]
+    #     print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
