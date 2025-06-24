8c3c31a0b04949469edff4c5f8b31c4460324327
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2025-06-23 20:51:00 -0400

    even more slurm stuff changes

---
+++ slurm/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --nodes=1             # nodes requested
+#SBATCH --ntasks=1            # tasks requested
+#SBATCH --cpus-per-task=8     # Specify the number of CPUs your task will need.
+#SBATCH --gres=gpu:1          # the number of GPUs requested
+#SBATCH --mem=50G             # memory 
+#SBATCH -o outfile.txt            # send stdout to outfile
+#SBATCH -e errfile.txt            # send stderr to errfile
+#SBATCH -t 00:01:00           # time requested in hour:minute:second
+#SBATCH --mail-type=all       # choice between begin, end, all to notify you via email
+#SBATCH --mail-user=se0361@princeton.edu
+
+conda activate sofia
+python ../training.py
