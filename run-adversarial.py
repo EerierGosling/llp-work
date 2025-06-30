87d1bf5df307c461c112cf5f9c88fc281a73e528
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2025-06-30 14:53:45 -0400

    fixing tab

--- run-adversarial.py
+++ run-adversarial.py
@@ -8,7 +8,7 @@ epsilon = [0.01, 0.1, 0.2]
 adversarial_ratio = [0.1, 0.2, 0.3]
 
 for i in range(9):
-    command = f"python adversarial.py --learning_rate {learning_rate} --weight_decay {weight_decay} --epsilon {epsilon[i % 3]} --adversarial_ratio {adversarial_ratio[i // 3]} --job_id {i + 1}"
+    command = f"python adversarial.py --learning_rate {learning_rate} --weight_decay {weight_decay} --epsilon {epsilon[i % 3]} --adversarial_ratio {adversarial_ratio[i // 3]}"
     print(f"Running command: {command}")
 
-os.system(command)
+    os.system(command)
